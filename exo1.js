efed191c112e9d6369a62679fdeb636f08fadc5f
Pedro De Almeida Rodrigues <pedrodealmeidarodrigues13@gmail.com>
2024-10-24 16:42:44 +0200

    début exo

---
+++ exo1.js
@@ -0,0 +1,9 @@
+// Exo1 //
+let a = 10
+let b = 10
+function multiply( a , b) {
+    return a * b;
+}
+
+let result = multiply(a, b)
+console.log(result);
---
+++ exo3.js
@@ -0,0 +1,3 @@
+function sumUpTo(n) {
+    let s
+}
