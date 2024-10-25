01bcfc61ba301c58e3aa2a95625cd3eaf9f0a2c3
Pedro De Almeida Rodrigues <pedrodealmeidarodrigues13@gmail.com>
2024-10-25 09:20:43 +0200

    fin exo fonction js

--- exo2.js
+++ exo2.js
@@ -1,9 +1,6 @@
 function isEven(number) {
-    if (number % 2 === 0) {
-        return true;
-    }else {
-        return false;
-    }
+    return number % 2 === 0
 }
+
 console.log(isEven(4));
 console.log(isEven(7));
---
+++ exo4.js
@@ -0,0 +1,6 @@
+function multiply(a, b) {
+    let product = a * b;
+    return product
+}
+
+console.log(multiply(2, 3));
